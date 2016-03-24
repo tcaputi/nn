3cc285bf94e05d369680e22c01fcd49465266202
Tom Caputi <tcaputi@datto.com>
2016-03-24 04:55:58 -0400

    separate nodes for each mnist output. it has gained sentience.

--- src/test.c
+++ src/test.c
@@ -8,7 +8,7 @@
 #define NR_INPUTS MNIST_IMAGE_SIZE
 #define HIDDEN_NPL 300
 #define HIDDEN_LAYERS 1
-#define NR_OUTPUTS 1
+#define NR_OUTPUTS 10
 
 #define LEARNING_RATE 0.025
 #define MOMENTUM 0.2
@@ -21,6 +21,36 @@ static double sigmoid(double value){
 
 static double sigmoid_derivative(double value){
 	return 1.0 - value * value;
+}
+
+static void print_top3(struct nn_node *output_nodes, unsigned nr_outputs){
+	int i;
+	int fidx, sidx, tidx;
+	double first = 0, second = 0, third = 0;
+	
+	for(i = 0; i < nr_outputs; i++){
+		if(output_nodes[i].output > first){
+			third = second;
+			tidx = sidx;
+			second = first;
+			sidx = fidx;
+			first = output_nodes[i].output;
+			fidx = i;
+		}else if(output_nodes[i].output > second){
+			third = second;
+			tidx = sidx;
+			second = output_nodes[i].output;
+			sidx = i;
+		}else if(output_nodes[i].output > third){
+			third = output_nodes[i].output;
+			tidx = i;
+		}
+	}
+	
+	printf("TOP 3 CHOICES:\n");
+	printf("\t%d : %f\n", fidx, first);
+	printf("\t%d : %f\n", sidx, second);
+	printf("\t%d : %f\n", tidx, third);
 }
 
 int main(int argc, char **argv){
@@ -49,21 +79,22 @@ int main(int argc, char **argv){
 		for(j = 0; j < MNIST_IMAGE_SIZE; j++){
 			input[j] = mnist->train->images[i].data[j] / 255.0;
 		}
-		expected[0] = mnist->train->labels[i] / 10.0;
+		
+		for(j = 0; j < 10; j++){
+			if(j == mnist->train->labels[i]) expected[j] = 1.0;
+			else expected[j] = 0.0;
+		}
 		
 		nn_array_network_process(&nn, input, expected, NN_MODE_TRAIN);
 		
 		if(i % PRINT_GENERTION == 0){
 			printf("TRAINING: generation %d\n", i);
-			printf("\tinput = %f\n", input[0]);
-			printf("\texpected = %f\n", expected[0]);
-			printf("\toutput = %f\n", nn.output_nodes[0].output);
+			printf("\texpected = %u\n", mnist->train->labels[i]);
+			print_top3(nn.output_nodes, NR_OUTPUTS);
 			printf("\terror (RMS) = %f\n", nn.error);
 			printf("\n");
 			mnist_image_print(&mnist->train->images[i]);
 			printf("\n");
-			//nn_node_printf(&nn.output_nodes[0]);
-			//printf("\n");
 			printf("---------------------------------------\n");
 		}
 	}
@@ -72,15 +103,22 @@ int main(int argc, char **argv){
 		for(j = 0; j < MNIST_IMAGE_SIZE; j++){
 			input[j] = mnist->test->images[i].data[j] / 255.0;
 		}
-		expected[0] = mnist->test->labels[i] / 10.0;
+		
+		for(j = 0; j < 10; j++){
+			if(j == mnist->train->labels[i]) expected[j] = 1.0;
+			else expected[j] = 0.0;
+		}
 		
 		nn_array_network_process(&nn, input, expected, NN_MODE_TRAIN);
 		
 		if(i % PRINT_GENERTION == 0){
 			printf("PREDICTION: generation %d\n", i);
-			printf("\texpected = %f\n", expected[0]);
-			printf("\toutput = %f\n", nn.output_nodes[0].output);
+			printf("\texpected = %u\n", mnist->train->labels[i]);
+			print_top3(nn.output_nodes, NR_OUTPUTS);
 			printf("\terror (RMS) = %f\n", nn.error);
+			printf("\n");
+			mnist_image_print(&mnist->train->images[i]);
+			printf("\n");
 			printf("---------------------------------------\n");
 		}
 	}
